35066905f1ae37424baaa4fd6da73e89865ad5dc
szymon klimczuk <szymon.klimczuk@eclipsegroup.co.uk>
2019-06-06 13:59:45 +0200

    simple form created, data from post captured

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def homepage():
+    return render_template("index.html")
+
+
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    if request.method == 'GET':
+        return render_template("result.html")
+    if request.method == 'POST':
+        sizeOfPopulation = request.form['sizeOfPopulation']
+        return render_template("result.html")
+    return render_template("result.html")
 
 
 if __name__ == '__main__':
