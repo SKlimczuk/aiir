fb832550199eaceabb8c870ae228c9e845cb97f7
szymon klimczuk <szymon.klimczuk@eclipsegroup.co.uk>
2019-06-06 15:40:01 +0200

    alg work update

--- Genetic.py
+++ Genetic.py
@@ -9,9 +9,13 @@ class Genetic:
         return generatedRadnomNumber
 
     def initRandomPopulation(self):
+        # sum = 0
         for i in range(self.generateRandomPopulationSize()):
             gen = Gen.Gen(i)
             self.population.append(gen)
+            # sum += gen.get_adaptation()
+
+        return self.population
 
     def initPopulation(self, limit):
         for i in range(limit):
@@ -51,11 +55,30 @@ class Genetic:
         sizeOfListToRank = len(listToRank)
         for i in range(sizeOfListToRank):
             for j in range(0, sizeOfListToRank - i - 1):
-                if listToRank[j] > listToRank[j + 1]:
+                if listToRank[j] < listToRank[j + 1]:
                     listToRank[j], listToRank[j + 1] = listToRank[j + 1], listToRank[j]
         return listToRank[:sizeOfListToRank - 1]
 
-    def runSingleStepOfAlghoritm(self, population):
-        size = len(population)
-        for i in range(size):
-            self.ranking(population[i])
+    def runAlgorithm(self, population, generations):
+        sizeOfPopulation = len(population)
+        for g in range(generations):
+            for i in range(sizeOfPopulation):
+                self.ranking(population[i])
+
+    def findTheStrongestPopulation(self, populations):
+        strongest = 0
+        for p in populations:
+            candidate = self.calcAdaptation(p)
+            if candidate >= strongest:
+                strongest = candidate
+        print('strongest population with adpt %d' % strongest)
+        return strongest
+
+    def calcAdaptation(self, population):
+        gen = Gen.Gen(0)
+        sizeOfPopulation = len(population)
+        sum = 0
+        for i in range(sizeOfPopulation):
+            gen = population[i]
+            sum += gen.get_adaptation()
+        return sum
--- main.py
+++ main.py
@@ -1,31 +1,37 @@
 from mpi4py import MPI
-
 import Genetic
 
 size = 100
 sizeOfRanges = 10
+generations = 5
 
 genetic = Genetic.Genetic()
 genetic.initPopulation(size)
-listOfRanges = genetic.dividePopulation(10)
+listOfDividedPopulations = genetic.dividePopulation(10)
+
+# genetic.findTheStrongestPopulation(listOfDividedPopulations)
 
 comm = MPI.COMM_WORLD
 rank = comm.rank
+name = MPI.Get_processor_name()
 
 sizeOfPopulationPerProcess = (size // sizeOfRanges) // (comm.size - 1)
 
 if rank == 0:
-    # print('size of pop per node', sizeOfPopulationPerProcess)
-    data1 = listOfRanges[:sizeOfPopulationPerProcess]
-    data2 = listOfRanges[sizeOfPopulationPerProcess:]
+    data1 = listOfDividedPopulations[:sizeOfPopulationPerProcess]
+    data2 = listOfDividedPopulations[sizeOfPopulationPerProcess:]
     comm.send(data1, dest=1)
     comm.send(data2, dest=2)
-    data1 = comm.recv(source=1)
-    data2 = comm.recv(source=2)
+    print('root rank', rank, ', with name', name)
+    genetic.findTheStrongestPopulation(listOfDividedPopulations)
 else:
+    print('slave rank', rank, ', with name', name)
     data = comm.recv(source=0)
-    genetic.runSingleStepOfAlghoritm(data)
-    comm.send(data, dest=0)
+    genetic.runAlgorithm(data, generations)
+    # for x in range(len(data)):
+    #     print(data[x])
+    # comm.send(data, dest=0)
+    comm.gather(data, root=0)
 
 # elif rank == 1:
 #     data = comm.recv(source=0)
